242b523986e2aa8431c6b758d70fcee8c7321a36
raiyanmook27 <raiyan.mook27@gmail.com>
2023-06-25 18:23:52 +0000

    FEAT: SECOND APP

--- my-first-react/src/App.js
+++ my-first-react/src/App.js
@@ -1,13 +1,24 @@
-import Todo from "./components/Todo";
+import { Route, Routes } from "react-router-dom"; // import Route which is a component
+//that we can use to define a route
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+import MainNav from "./components/layout/MainNav";
+
+// switch is a component that allows us to define a group of routes and
+//it will make sure that only one of these routes is loaded at a time
+//so that we don't have multiple pages loaded at the same time
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
-      <div className="card">
-        <Todo text="Learn React" />
-        <Todo text="Learn Blockchain" />
-        <Todo text="Learn Off-Chain" />
-      </div>
+      <MainNav />
+      <switch>
+        <Routes>
+          <Route path="/" element={<AllMeetupsPage />} />
+          <Route path="/new-meetup" element={<NewMeetupPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+        </Routes>
+      </switch>
     </div>
   );
 }
